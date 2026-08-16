a0b3cec2a47ca23b2b5b0a66c455776ce2f6d8ea
ibrahimhussein123 <ibrahimhussein@aucegypt.edu>
2026-08-16 23:42:00 +0300

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// vite.config.js
+
 export default defineConfig({
   plugins: [react()],
   base: '/my-store-frontend/',
