3ba73fa2232db44cf1946cd9880a0291dfcc65f1
ibrahimhussein123 <ibrahimhussein@aucegypt.edu>
2026-08-16 23:09:37 +0300

    Delete all files

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-// vite.config.js
-export default defineConfig({
-  plugins: [react()],
-  base: '/my-store-frontend/',
-})
